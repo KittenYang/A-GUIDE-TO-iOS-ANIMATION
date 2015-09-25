4b18fdc4e015072e363c98f7569931c138f7f49e
KittenYang <kittenyang@icloud.com>
2015-09-26 03:06:19 +0800

    Add a custom loader animation KYLoadingHUD

---
+++ KYLoadingHUD/KYLoadingHUD.h
@@ -0,0 +1,16 @@
+//
+//  KYLoadingHUD.h
+//  
+//
+//  Created by Kitten Yang on 2/23/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYLoadingHUD : UIView
+
+
+-(void)showHUD;
+-(void)dismissHUD;
+@end
