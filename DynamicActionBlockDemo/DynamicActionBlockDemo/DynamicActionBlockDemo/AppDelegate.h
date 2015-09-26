26aa21e9a1277062a45453e558a9c123cb47c0a3
KittenYang <kittenyang@icloud.com>
2015-09-27 01:10:05 +0800

    Add DynamicActionBlockDemo

---
+++ DynamicActionBlockDemo/DynamicActionBlockDemo/DynamicActionBlockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicActionBlockDemo
+//
+//  Created by Kitten Yang on 9/27/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- ios-ChristmasBell-master/README.md
+++
@@ -1,4 +0,0 @@
-ios-ChristmasBell
-=================
-
-An example of CAEmitterLayer. Also used UIKit Dynamics and CAShapeLayer for String like behavior of UIAttachmentBehavior
