cc3cca0d26a95f4ee99c638c8153a7607c7aa20f
KittenYang <kittenyang@icloud.com>
2016-01-02 13:39:23 +0800

    Reconsitution DynamicActionBlockDemo Objective-C version.

---
+++ Objective-C Version/DynamicActionBlockDemo/DynamicActionBlockDemo/DynamicActionBlockDemo/DynamicView.h
@@ -0,0 +1,13 @@
+//
+//  DynamicView.h
+//  DynamicActionBlockDemo
+//
+//  Created by Kitten Yang on 1/2/16.
+//  Copyright © 2016 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicView : UIView
+
+@end
---
+++ Swift Version/DynamicActionBlockDemo-Swift/DynamicActionBlockDemo-SwiftTests/DynamicActionBlockDemo_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  DynamicActionBlockDemo_SwiftTests.swift
+//  DynamicActionBlockDemo-SwiftTests
+//
+//  Created by Kitten Yang on 1/2/16.
+//  Copyright © 2016 Kitten Yang. All rights reserved.
+//
+
+import XCTest
+@testable import DynamicActionBlockDemo_Swift
+
+class DynamicActionBlockDemo_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
