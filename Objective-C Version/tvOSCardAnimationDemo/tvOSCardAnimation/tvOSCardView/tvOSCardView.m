d081ed25d781bb312b69c030aca2149185517d62
KittenYang <kittenyang@icloud.com>
2016-01-15 23:22:11 +0800

    Initialize tvOSCardAnimation-Swift

--- Objective-C Version/tvOSCardAnimationDemo/tvOSCardAnimation/tvOSCardView/tvOSCardView.m
+++ Objective-C Version/tvOSCardAnimationDemo/tvOSCardAnimation/tvOSCardView/tvOSCardView.m
@@ -18,22 +18,16 @@
 -(id)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
-
     }
     return self;
-    
 }
-
 
 -(id)init{
     self = [super init];
     if (self) {
-
     }
     return self;
-    
 }
-
 
 -(id)initWithCoder:(NSCoder *)aDecoder{
     self = [super initWithCoder:aDecoder];
@@ -41,17 +35,16 @@
 
     }
     return self;
-    
 }
 
 -(void)willMoveToSuperview:(UIView *)newSuperview{
     
     [super willMoveToSuperview:newSuperview];
     [self setUpSomething];
-    
 }
 
 #pragma mark -- helper
+
 -(void)setUpSomething{
 
     self.layer.shadowColor = [UIColor blackColor].CGColor;
@@ -59,7 +52,6 @@
     self.layer.shadowRadius = 10.0f;
     self.layer.shadowOpacity = 0.3f;
     
-
     cardImageView = [[UIImageView alloc]initWithFrame:self.bounds];
     cardImageView.image = [UIImage imageNamed:@"poster"];
     cardImageView.layer.cornerRadius = 5.0f;
@@ -69,16 +61,12 @@
     UIPanGestureRecognizer *panGes = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panInCard:)];
     [self addGestureRecognizer:panGes];
     
-    
     cardParallaxView = [[UIImageView alloc]initWithFrame:cardImageView.frame];
     cardParallaxView.image = [UIImage imageNamed:@"5"];
     cardParallaxView.layer.transform = CATransform3DTranslate(cardParallaxView.layer.transform, 0, 0, 200);
     [self insertSubview:cardParallaxView aboveSubview:cardImageView];
-    
 
 }
-
-
 
 -(void)panInCard:(UIPanGestureRecognizer *)panGes{
     
@@ -86,18 +74,14 @@
     
     if (panGes.state == UIGestureRecognizerStateChanged) {
         
-        
         CGFloat xFactor = MIN(1, MAX(-1,(touchPoint.x - (self.bounds.size.width/2)) / (self.bounds.size.width/2)));
         CGFloat yFactor = MIN(1, MAX(-1,(touchPoint.y - (self.bounds.size.height/2)) / (self.bounds.size.height/2)));
-        
         
         cardImageView.layer.transform = [self transformWithM34:1.0/-500 xf:xFactor yf:yFactor];
         cardParallaxView.layer.transform = [self transformWithM34:1.0/-250 xf:xFactor yf:yFactor];
         
-        
         CGFloat zFactor = 180 * atan(yFactor/xFactor) / M_PI+90;
         NSLog(@"%f",zFactor);
-        
         
     }else if (panGes.state == UIGestureRecognizerStateEnded){
         
@@ -105,7 +89,6 @@
             cardImageView.layer.transform = CATransform3DIdentity;
             cardParallaxView.layer.transform = CATransform3DIdentity;
         } completion:NULL];
-        
     }
     
 }
